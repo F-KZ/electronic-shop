8d943f06b3edc130397d70e6687509815814ec66
F-KZ <supreme-soul@hotmail.fr>
2024-07-29 12:44:59 +0200

    test5

--- back/index.js
+++ back/index.js
@@ -23,9 +23,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); 
 // CORS configuration
-//const allowedOrigins = ['https://electronic-shop-back.vercel.app', 'http://localhost:5173'];
-app.use(cors());
+const allowedOrigins = ['https://electronic-shop-back.vercel.app', 'http://localhost:5173'];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true, // Allow cookies and credentials to be sent
+};
+
+app.use(cors(corsOptions));
 //Routes
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes);
